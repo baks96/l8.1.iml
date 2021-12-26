f8595fa84762edb818bc162b535c40179f979ac2
baks96 <kudakeevbakai@gmail.com>
2021-12-26 18:57:53 +0600

    Initial commit

---
+++ src/kg/geektech/game/players/Avrora.java
@@ -0,0 +1,12 @@
+package kg.geektech.game.players;
+
+public class Avrora extends Hero {
+    public Avrora(int health, int damage, SuperAbility superAbility, String name) {
+        super(health, damage, superAbility, name);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+
+    }
+}
